1374b4d52a934e9c305ad5434ac62d55c4c38efa
Freyyngling <102222351+Freyyngling@users.noreply.github.com>
2026-06-08 21:54:44 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -46,7 +46,7 @@ function injectStyles() {
       position: fixed;
       bottom: 60px;
       left: 20px;
-      width: 390px;
+      width: 450px;
       z-index: 9500;
       display: none;
       flex-direction: column;
@@ -290,6 +290,7 @@ function buildChatUI() {
     <div id="chat-window">
       <div id="chat-header">
         <img id="chat-header-avatar" src="front-smile.png" alt="avatar">
+        <div id="chat-header-info">
 
     <div id="chat-header-info">
 
